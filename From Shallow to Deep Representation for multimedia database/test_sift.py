3e13e7a4a00765e9316c31bb20c2b85fe20d76ef
jnguyen1192 <johdu13@hotmail.fr>
2018-12-23 17:40:28 +0100

    Feature matching and geometric transformation images

--- From Shallow to Deep Representation for multimedia database/test_sift.py
+++ From Shallow to Deep Representation for multimedia database/test_sift.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class TestSift(unittest.TestCase):
+
+    # PART 1
     def test_sift_keypoints(self):
         import numpy as np
         import cv2 as cv
@@ -13,7 +15,7 @@ class TestSift(unittest.TestCase):
         sift = cv.xfeatures2d.SIFT_create()
         kp = sift.detect(gray, None)
         img = cv.drawKeypoints(gray, kp, img)
-        cv.imwrite('fly_sift_keypoints.jpg', img)
+        cv.imwrite('sift/fly_sift_keypoints.jpg', img)
 
     def test_sift_keypoints_rich(self):
         import numpy as np
@@ -23,23 +25,25 @@ class TestSift(unittest.TestCase):
         sift = cv.xfeatures2d.SIFT_create()
         kp = sift.detect(gray, None)
         img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv.imwrite('sift_rich_keypoints.jpg', img)
+        cv.imwrite('sift/sift_rich_keypoints.jpg', img)
 
-# TODO Now to calculate the descriptor, OpenCV provides two methods.
+# Now to calculate the descriptor, OpenCV provides two methods.
     def test_sift_descriptor_1(self):
         import numpy as np
         import cv2 as cv
         img = cv.imread('fly.jpg')
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         sift = cv.xfeatures2d.SIFT_create()
-        # keypoints and descriptor
-        kp = sift.detect(gray, None)
-        kp, des = sift.compute(gray, kp)
-        print(len(kp))
-        print(len(des))
         #Since you already found keypoints, you can call sift.compute() which
         #       computes the descriptors from the keypoints we have found.
         #       Eg: kp,des = sift.compute(gray,kp)
+        kp = sift.detect(gray, None)
+        kp, des = sift.compute(gray, kp)
+        #img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        #cv.imwrite('sift_rich_keypoints_descriptor_1.jpg', img)
+        print("First method ")
+        print("kp ", len(kp))
+        print("des ", len(des))
 
     def test_sift_descriptor_2(self):
         import numpy as np
@@ -50,14 +54,11 @@ class TestSift(unittest.TestCase):
         #If you didn't find keypoints, directly find keypoints and descriptors
         #       in a single step with the function, sift.detectAndCompute().
         kp, des = sift.detectAndCompute(gray, None)
-        img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv.imwrite('sift_rich_keypoints_descriptor.jpg', img)
-        print(len(kp))
-        print(len(des))
-
-
-
-
+        #img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        #cv.imwrite('sift_rich_keypoints_descriptor_2.jpg', img)
+        print("Second method ")
+        print("kp ", len(kp))
+        print("des ", len(des))
 
 
 if __name__ == '__main__':
--- From Shallow to Deep Representation for multimedia database/test_surf.py
+++ From Shallow to Deep Representation for multimedia database/test_surf.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 
 class TestStringMethods(unittest.TestCase):
+
+    # PART 2
     def test_len_descriptor(self):
         img = cv.imread('fly.png', 0)
         # Create SURF object. You can specify params here or later.
@@ -49,6 +51,47 @@ class TestStringMethods(unittest.TestCase):
         #print(len(kp))
         img2 = cv.drawKeypoints(img, kp, None, (255, 0, 0), 4)
         plt.imshow(img2), plt.show()
+
+    def test_u_surf(self):
+        img = cv.imread('fly.png', 0)
+        # Create SURF object. You can specify params here or later.
+        # Here I set Hessian Threshold to 400
+        surf = cv.xfeatures2d.SURF_create(400)
+        # Find keypoints and descriptors directly
+        kp, des = surf.detectAndCompute(img, None)
+
+        # Check upright flag, if it False, set it to True
+        print(surf.getUpright())
+        surf.setUpright(True)
+        # Recompute the feature points and draw it
+        kp = surf.detect(img, None)
+        img2 = cv.drawKeypoints(img, kp, None, (255, 0, 0), 4)
+        plt.imshow(img2), plt.show()
+
+    def test_descriptor_size(self):
+        img = cv.imread('fly.png', 0)
+        # Create SURF object. You can specify params here or later.
+        # Here I set Hessian Threshold to 400
+        surf = cv.xfeatures2d.SURF_create(400)
+        # Find keypoints and descriptors directly
+        kp, des = surf.detectAndCompute(img, None)
+
+        # Check upright flag, if it False, set it to True
+        print(surf.getUpright())
+        surf.setUpright(True)
+
+        # Find size of descriptor
+        print(surf.descriptorSize())
+
+        # That means flag, "extended" is False.
+        surf.getExtended()
+
+        # So we make it to True to get 128-dim descriptors.
+        surf.setExtended(True)
+        kp, des = surf.detectAndCompute(img, None)
+        print(surf.descriptorSize())
+        print(des.shape)
+
 
 
 
