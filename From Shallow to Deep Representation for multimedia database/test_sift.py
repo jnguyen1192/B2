4cccf5f7d72178465047ec2cbe29efb91f8c6500
jnguyen1192 <johdu13@hotmail.fr>
2018-12-23 13:58:26 +0100

    Two method to obtain Sift descriptor done

--- From Shallow to Deep Representation for multimedia database/test_sift.py
+++ From Shallow to Deep Representation for multimedia database/test_sift.py
@@ -25,22 +25,35 @@ class TestSift(unittest.TestCase):
         img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv.imwrite('sift_rich_keypoints.jpg', img)
 
-    def test_sift_descriptor(self):
+# TODO Now to calculate the descriptor, OpenCV provides two methods.
+    def test_sift_descriptor_1(self):
         import numpy as np
         import cv2 as cv
         img = cv.imread('fly.jpg')
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         sift = cv.xfeatures2d.SIFT_create()
+        # keypoints and descriptor
         kp = sift.detect(gray, None)
-        img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv.imwrite('sift_rich_keypoints.jpg', img)
-
-        #TODO Now to calculate the descriptor, OpenCV provides two methods.
+        kp, des = sift.compute(gray, kp)
+        print(len(kp))
+        print(len(des))
         #Since you already found keypoints, you can call sift.compute() which
         #       computes the descriptors from the keypoints we have found.
         #       Eg: kp,des = sift.compute(gray,kp)
+
+    def test_sift_descriptor_2(self):
+        import numpy as np
+        import cv2 as cv
+        img = cv.imread('fly.jpg')
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        sift = cv.xfeatures2d.SIFT_create()
         #If you didn't find keypoints, directly find keypoints and descriptors
         #       in a single step with the function, sift.detectAndCompute().
+        kp, des = sift.detectAndCompute(gray, None)
+        img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv.imwrite('sift_rich_keypoints_descriptor.jpg', img)
+        print(len(kp))
+        print(len(des))
 
 
 
