42b65fab132cdc15f7e7801d8bb0ac1f7d30288b
jnguyen1192 <johdu13@hotmail.fr>
2018-12-27 12:59:15 +0100

    Add image on specific cluster directory

--- From Shallow to Deep Representation for multimedia database/test_cluster_image.py
+++ From Shallow to Deep Representation for multimedia database/test_cluster_image.py
@@ -14,6 +14,17 @@ class TestClusterImage(unittest.TestCase):
         path = 'cluster/1'
         os.makedirs(path, exist_ok=True)
 
+    def test_add_image_on_directory(self):
+        # add an image on a specific directory
+        import os
+        base_name = '122302.jpg'
+        holiday_directory = 'holiday_dataset'
+        cluster_directory = 'cluster/1'
+        path_holiday = os.path.join(holiday_directory, base_name)
+        img = cv.imread(path_holiday, 0)  # trainImage
+        path_cluster = os.path.join(cluster_directory, base_name)
+        cv.imwrite(path_cluster, img)
+
 
 if __name__ == '__main__':
     unittest.main()
