fbbf85cfce1645ecd75017d635f28a71b3f3db1f
jnguyen1192 <johdu13@hotmail.fr>
2019-01-02 19:12:18 +0100

    Other cluster added using postgres database with FLANN's matcher

--- FromShallowToDeepRepresentationForMultimediaDatabase/postgres_database.py
+++ FromShallowToDeepRepresentationForMultimediaDatabase/postgres_database.py
@@ -69,7 +69,16 @@ def _adapt_array(text):
     outfile = TemporaryFile()
     np.savetxt(outfile, text)
     outfile.seek(0)
-    return outfile.read()
+    return str(outfile.read())[2:-1].replace("\\n", "\n")
+
+
+# converts from postgres to python
+def _typecast_array(string):
+    from tempfile import TemporaryFile
+    outfile = TemporaryFile()
+    outfile.write(string.encode())
+    outfile.seek(0)  # Only needed here to simulate closing & reopening file
+    return np.loadtxt(outfile, dtype=np.float32)
 
 
 def insert_cluster_with_descriptor(num_cluster, des):
@@ -84,7 +93,7 @@ def insert_cluster_with_descriptor(num_cluster, des):
 
         # create a cursor
         cur = conn.cursor()
-
+        #print(_adapt_array(des)[:10])
         # execute a statement
         cur.execute("INSERT INTO cluster(num_cluster, des) VALUES (%s, %s)", (num_cluster, _adapt_array(des)))
         conn.commit()
@@ -151,6 +160,36 @@ def get_number_cluster():
         if conn is not None:
             conn.close()
     return result[0]
+
+
+def get_descriptor_cluster_with_num_cluster(num_cluster):
+    """ insert image into the PostgreSQL database server """
+    conn = None
+    result = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        cur.execute("SELECT des FROM cluster WHERE num_cluster=%s", (num_cluster,))
+
+        result = cur.fetchone()
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+    #print(result)
+    return _typecast_array(result[0])
 
 
 if __name__ == '__main__':
--- FromShallowToDeepRepresentationForMultimediaDatabase/test_postgres_cluster_image.py
+++ FromShallowToDeepRepresentationForMultimediaDatabase/test_postgres_cluster_image.py
@@ -102,11 +102,37 @@ class TestPostgresClusterImage(unittest.TestCase):
             print("Cluster exist")
 
     def test_case_other_cluster(self):
-        from postgres_database import get_number_cluster, insert_cluster_with_descriptor, insert_has_cluster_with_num_cluster_and_name_image
+        from postgres_database import get_number_cluster, insert_cluster_with_descriptor, insert_has_cluster_with_num_cluster_and_name_image, get_descriptor_cluster_with_num_cluster
         import os
 
+        def is_match_cluster(des1, des2, threshold=400):
+            """
+            Know if the descriptor matches with our cluster
+            :param des1: the first descriptor to test
+            :param des2: the second descriptor to test
+            :param threshold: the threshold for the match
+            :return: True if they match
+            """
+            # FLANN parameters
+            FLANN_INDEX_KDTREE = 1
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)  # or pass empty dictionary
+            flann = cv.FlannBasedMatcher(index_params, search_params)
+            print('des1 ', des1.shape)
+            print('des2 ', des2.shape)
+            matches = flann.knnMatch(des1, des2, k=2)
+            # Need to draw only good matches, so create a mask
+            matchesMask = [[0, 0] for i in range(len(matches))]
+            count = 0
+            # ratio test as per Lowe's paper
+            for i, (m, n) in enumerate(matches):
+                if m.distance < 0.7 * n.distance:
+                    matchesMask[i] = [1, 0]
+                    count += 1
+            return count > threshold
+
         number_cluster = get_number_cluster()
-        base_name = '122302.jpg'
+        base_name = '122400.jpg'  # 122302 122400
         holiday_directory = 'holiday_dataset'
         path_holiday = os.path.join(holiday_directory, base_name)
         # trainImage
@@ -114,15 +140,36 @@ class TestPostgresClusterImage(unittest.TestCase):
         # Initiate SIFT detector
         sift = cv.xfeatures2d.SIFT_create()
         # find the keypoints and descriptors with SIFT
-        kp, des = sift.detectAndCompute(img, None)
+        kp, des1 = sift.detectAndCompute(img, None)
         if number_cluster == 0:
             print("No cluster, we will create our first cluster")
             # use the descriptor to add the new cluster row
-            insert_cluster_with_descriptor(1, des)
+            insert_cluster_with_descriptor(1, des1)
             # use the path of image to add the new has_cluster row
             insert_has_cluster_with_num_cluster_and_name_image(1, path_holiday)
         else:
             print("Cluster exist")
+            # we verify if the descriptor will match with one of the descriptor of each cluster
+            is_cluster_match = False
+            for num_cluster in range(1, number_cluster+1):
+                des2 = get_descriptor_cluster_with_num_cluster(num_cluster)
+                #print("des2 ", des2)
+                # if match add to the cluster and break
+                if is_match_cluster(des1, des2):
+                    is_cluster_match = True
+                    # use the path of image to add the current cluster with has_cluster row
+                    insert_has_cluster_with_num_cluster_and_name_image(num_cluster, path_holiday)
+                    break
+            # else create a new cluster
+            if not is_cluster_match:
+                # use the descriptor to add the new cluster row
+                insert_cluster_with_descriptor(number_cluster+1, des1)
+                # use the path of image to add the new has_cluster row
+                insert_has_cluster_with_num_cluster_and_name_image(number_cluster+1, path_holiday)
+
+    def test_get_descriptor_from_cluster_using_num_cluster(self):
+        from postgres_database import get_descriptor_cluster_with_num_cluster
+        print(get_descriptor_cluster_with_num_cluster(1))
 
     def test_np_array_string(self):
         # https://stackoverflow.com/questions/10529351/using-a-psycopg2-converter-to-retrieve-bytea-data-from-postgresql
@@ -145,7 +192,6 @@ class TestPostgresClusterImage(unittest.TestCase):
             outfile.seek(0)  # Only needed here to simulate closing & reopening file
             return np.loadtxt(outfile)
 
-        
         base_name = '122302.jpg'
         holiday_directory = 'holiday_dataset'
         path_holiday = os.path.join(holiday_directory, base_name)
@@ -157,10 +203,10 @@ class TestPostgresClusterImage(unittest.TestCase):
         kp, des = sift.detectAndCompute(img, None)
 
         my_string = _adapt_array(des)
-        print("des ", des)
+        print("my_string ", my_string)
 
         new_array = _typecast_array(my_string)
-        print("new_array ", new_array)
+        #print("new_array ", new_array)
 
     def test_cluster_image(self):
         # create the cluster class
