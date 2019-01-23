a7268ffb4d7bb9c57f6cbded4d515dffe53104e4
jnguyen1192 <johdu13@hotmail.fr>
2019-01-23 13:32:26 +0100

    Default Changelist

---
+++ DataMining/Lab 3 Clustering/download_datasets.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00476/buddymove_holidayiq.csv
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00216/amzn-anon-access-samples.tgz
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/bag-of-words/
