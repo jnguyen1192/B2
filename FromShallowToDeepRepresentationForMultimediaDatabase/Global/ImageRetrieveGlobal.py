4639a60d7fe755afa12e33850d05bb9430ffae51
jnguyen1192 <johdu13@hotmail.fr>
2019-01-28 21:53:21 +0100

    #4 Preparation Bag of Words

---
+++ FromShallowToDeepRepresentationForMultimediaDatabase/Global/ImageRetrieveGlobal.py
@@ -0,0 +1,149 @@
+import cv2 as cv
+import numpy as np
+import logging
+import os
+import glob
+
+
+class ImageRetrieveGlobal:
+    def __init__(self, path_imgs):
+        self.nb_cluster = 0
+        self.path_imgs = path_imgs
+
+    def incr_nb_cluster(self):
+        """
+        Increment the number of current cluster
+        """
+        self.nb_cluster = self.nb_cluster + 1
+
+    def holiday_images(self, number_img):
+        """
+        Get the first name of image on the directory specified
+        :param number: number of name image
+        :param directory: path of the directory
+        :return:
+        """
+        image_list = []
+        for index, filename in enumerate(glob.glob(self.path_imgs + '/*.jpg')):  # assuming gif
+            image_list.append(filename)
+            if index >= number_img-1:
+                break
+        return image_list
+
+    def extract_descriptor_from_path_img(self, path_img):
+        """
+        Get the descriptor from the path of the img
+        :param path_img: the path of the descriptor
+        :return: the descriptor
+        """
+        # Initiate SIFT detector
+        sift = cv.xfeatures2d.SIFT_create()
+        img = cv.imread(path_img, 0)
+        # find the keypoints and descriptors with SIFT
+        kp, des = sift.detectAndCompute(img, None)
+        return des
+
+    def save_des_with_img_into_new_cluster(self, des, path_img):
+        """
+        Save the descriptor and the img using path of the image
+        into a new cluster.
+        :param des: the descriptor as a numpy array
+        :param path_img: the path of the image
+        :return: 0 if the save works else -1
+        """
+        try:
+            # create a new directory with the nb of cluster
+            os.system("mkdir -p " + str(self.nb_cluster))
+            # add a file with the descriptor on this directory
+            np.savetxt(str(self.nb_cluster) + "/des", des)
+            # add a file with the img on this directory using the path of the img
+            os.system("cp " + path_img + " " + str(self.nb_cluster) + "/" + path_img.split("/")[-1])
+            self.incr_nb_cluster()
+        except:
+            logging.ERROR("Save not working on")
+            return -1
+        return 0
+
+    def get_des_from_path_des(self, path_des):
+        """
+        Get the descriptor from the path
+        :param path_des: the path of the descriptor
+        :return: the descriptor
+        """
+        return np.loadtxt(path_des, dtype=np.float32)
+
+    def compare_path_des_and_des(self, path_des1,  des2, threshold=400):
+        """
+        Compare the two descriptor when the result is between the minimum threshold and the maximum threshold
+        :param desc1: the first descriptor
+        :param desc2:   the second descriptor
+        :return: True if they matched else False
+        """
+        # FLANN parameters
+        FLANN_INDEX_KDTREE = 1
+        count = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)  # or pass empty dictionary
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        # get des using path
+        des1 = self.get_des_from_path_des(path_des1)
+        # create the fast knn matcher
+        matches = flann.knnMatch(des1, des2, k=2)
+        # Need to draw only good matches, so create a mask
+        matchesMask = [[0, 0] for i in range(len(matches))]
+        # ratio test as per Lowe's paper
+        for i, (m, n) in enumerate(matches):
+            if m.distance < 0.7 * n.distance:
+                matchesMask[i] = [1, 0]
+                count += 1
+        return count > threshold
+
+    def add_img_on_cluster(self, path_img, num_cluster):
+        """
+        Add a new image on the specific cluster.
+        :param path_img: the path of the img
+        :param num_cluster: the number of the cluster we will add the image
+        :return: 0 if it works else -1
+        """
+        try:
+            # add a file with the img on this directory using the path of the img
+            os.system("cp " + path_img + " " + str(num_cluster) + "/" + path_img.split("/")[-1])
+        except:
+            logging.ERROR("Add on cluster " + str(num_cluster) + " not working on")
+            return -1
+        return 0
+
+    def exec(self, nb_img=50, threshold=400):
+        """
+        Execute the algorithm to create the cluster with each image,
+        each cluster has only one descriptor.
+        :param nb_img: the number of image we will use
+        :return: 0 if it works else -1
+        """
+        try:
+            # browse images
+            path_imgs = self.holiday_images(nb_img)
+
+            for path_img in path_imgs:
+                # get the descriptor of the current image
+                des1 = self.extract_descriptor_from_path_img(path_img)
+                if self.nb_cluster == 0:
+                    # add the first cluster
+                    self.save_des_with_img_into_new_cluster(des1, path_img)
+                    continue
+                cluster_match = False
+                for i in range(self.nb_cluster):
+                    path_des = str(i) + "/des"
+                    #print(path_des)
+                    if self.compare_path_des_and_des(path_des, des1, threshold):
+                        self.add_img_on_cluster(path_img, i)
+                        cluster_match = True
+                        break
+                # case not match create a new cluster
+                if not cluster_match:
+                    self.save_des_with_img_into_new_cluster(des1, path_img)
+        except:
+            logging.ERROR("Exec not working")
+            return -1
+        return 0
+
---
+++ FromShallowToDeepRepresentationForMultimediaDatabase/Global/Test/test_image_retrieval_global.py
@@ -0,0 +1,115 @@
+import unittest
+
+import os
+
+import numpy as np
+import cv2 as cv
+from ImageRetrieveGlobal import ImageRetrieveGlobal
+
+
+class TestImageRetrievalLocal(unittest.TestCase):
+
+    def test_0_code(self):
+        path_img = "../../cluster_res_1/0/126400.jpg"
+        os.system("mkdir -p " + str(0))
+        # Initiate SIFT detector
+        sift = cv.xfeatures2d.SIFT_create()
+        img = cv.imread(path_img, 0)
+        # find the keypoints and descriptors with SIFT
+        kp, des = sift.detectAndCompute(img, None)
+        np.savetxt("0/des", des)
+        assert(True)
+
+    def test_extract_descriptor_from_path_img(self):
+        """
+        Test if the function extract descriptor from path image
+        return the correct descriptor.
+        """
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+
+        path_img = "../../cluster_res_1/0/126400.jpg"
+
+        des1 = irl.extract_descriptor_from_path_img(path_img)
+        des2 = irl.get_des_from_path_des("0/des")
+
+        assert(np.array_equal(des1, des2))
+
+    def test_save_des_with_img_into_new_cluster(self):
+        """
+        Test if the function to save descriptor and image on a new directory works.
+        """
+        irl = ImageRetrieveGlobal("../holiday_dataset")
+        path_img = "../../cluster_res_1/0/126400.jpg"
+
+        des = irl.extract_descriptor_from_path_img(path_img)
+
+        assert(irl.save_des_with_img_into_new_cluster(des, path_img) == 0)
+
+    def test_get_des_from_path_des(self):
+        """
+        Test if the function to get a descriptor using a path of the file containing
+        the descriptor.
+        """
+        path_img = "../../cluster_res_1/0/126400.jpg"
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+        path_des = str(0) + "/des"
+
+        des1 = irl.get_des_from_path_des(path_des)
+        des2 = irl.extract_descriptor_from_path_img(path_img)
+
+        assert(np.array_equal(des1, des2))
+
+    def test_compare_path_des_and_des_true(self):
+        """
+        Test if the function compare descriptor works with true case.
+        """
+        irl = ImageRetrieveGlobal("../holiday_dataset")
+
+        path_des = str(0) + "/des"
+        path_img = "../../cluster_res_1/0/126401.jpg"
+
+        des = irl.extract_descriptor_from_path_img(path_img)
+
+        assert(irl.compare_path_des_and_des(path_des, des))
+
+    def test_compare_path_des_and_des_false(self):
+        """
+        Test if the function compare descriptor works with false case.
+        """
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+
+        path_des = str(0) + "/des"
+        path_img = "../../cluster_res_1/1/126402.jpg"
+
+        des = irl.extract_descriptor_from_path_img(path_img)
+
+        assert(not irl.compare_path_des_and_des(path_des, des))
+
+    def test_add_img_on_cluster(self):
+        """
+        Test if the image is add on the cluster
+        """
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+        path_img = "../../cluster_res_1/0/126401.jpg"
+        num_cluster = 0
+        assert(irl.add_img_on_cluster(path_img, num_cluster) == 0)
+
+    def test_holiday_images(self):
+        """
+        Test if 50 images are selected
+        """
+        nb_imgs = 50
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+        image_list = irl.holiday_images(nb_imgs)
+        assert(len(image_list) == nb_imgs)
+
+    def test_exec(self):
+        """
+        Test if execution algorithm to retrieve local image works.
+        """
+        irl = ImageRetrieveGlobal("../../holiday_dataset")
+        assert(irl.exec(812, 250) == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
