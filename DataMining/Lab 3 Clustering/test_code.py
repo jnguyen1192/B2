af1cc37b5f7fc9e4d0477b6a5f4ce71f910fc581
jnguyen1192 <johdu13@hotmail.fr>
2019-01-28 07:10:45 +0100

    #3 read CSV file

--- DataMining/Lab 3 Clustering/test_code.py
+++ DataMining/Lab 3 Clustering/test_code.py
@@ -1,11 +1,37 @@
 import unittest
 
 import sklearn
+import csv
 
 
+class TestClustering(unittest.TestCase):
 
-class TestCode(unittest.TestCase):
+    def test_prepare_dataset(self):
+        def readCsv(file, header=False, delimiter=',', newline='\n'):
+            """
+            Read the CSV file with the hearder or not
+            :param file: the path of the file
+            :param header: if there was a header on the file
+            :param delimiter: the delimter between each columns
+            :param delimiter: the delimter between each columns
+            :return: the array containing the values of the dataset
+            """
+            if not header:
+                c = 0
+            else:
+                c = 1
+            with open(file, newline=newline) as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=delimiter)
+                for row in spamreader:
+                    if c == 0:
+                        c = 1
+                        continue
+                    yield row
 
-    def test_inspyred(self):
+        data = readCsv('dataset/buddymove_holidayiq.csv', True)
+        for d in data:
+            print(d)
 
 
+if __name__ == '__main__':
+    unittest.main()
